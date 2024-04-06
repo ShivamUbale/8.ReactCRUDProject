a4ebbccad1f959c2e7daa905c9d018b29c42f203
Shivam <shivamubale3111@gmail.com>
2024-04-06 11:59:35 +0530

    updating context component

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -11,18 +11,22 @@ function Context(props) {
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [products, setProducts] = useState(JSON.parse(localStorage.getItem('products')) || null);
 
-    // const getProducts = async () => {
-    //     try {
-    //         const { data } = await axios('/products');
-    //         setProducts(data);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const { data } = await axios('/products');
+                setProducts(data);
+                localStorage.setItem('products', JSON.stringify(data));
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
 
-    //     useEffect(() => {
-    //         getProducts();
-    //     }, [])
+        // Fetch products only if they are not already present in local storage
+        if (!products) {
+            fetchProducts();
+        }
+    }, []);
 
 
         return (
